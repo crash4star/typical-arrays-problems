099acc24875bfc7f6b9c6092fda17614ade5a757
crash4star <47321264+crash4star@users.noreply.github.com>
2020-03-19 17:39:36 +0300

    feat: complete typical-array-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
+exports.min = function min(array) {
+    if (array == undefined) {
+        return 0;
+    } else {
+        let min = 0;
+        array.forEach((item, i, a) => {
+            if (a[i] < min) {
+                min = item;
+            }
+        });
 
-exports.min = function min (array) {
-  return 0;
+        return min;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    let positiveArr = [];
+
+    if (array == undefined || array.length == 0) {
+        return 0;
+    } else {
+        array.forEach((item, i, a) => {
+            if (item >= 0) {
+                positiveArr.push(item)
+            }
+        });
+    }
+
+    let max = positiveArr[0];
+
+    positiveArr.forEach((item, i, a) => {
+        if (item > max) {
+            max = item;
+        }
+    });
+
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let num = 0;
+
+    if (array == undefined || array.length == 0) {
+        return 0;
+    } else {
+        array.forEach(item => num += item);
+        return num / array.length;
+    }
 }
